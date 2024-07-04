106eb1d63f7379fe900c75de5cd779ee608d3e04
leticiafdepaula <leticiaferreira150998@gmail.com>
2024-07-04 19:07:48 -0300

    docker config

---
+++ src/main/java/com/lvendas/gestao_vendas/GestaoVendasApplication.java
@@ -0,0 +1,13 @@
+package com.lvendas.gestao_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lvendas/gestao_vendas/GestaoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lvendas.gestao_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
