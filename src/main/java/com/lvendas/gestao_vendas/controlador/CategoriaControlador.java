d56a938b235accab3f6d859cd3e2c9d53ca2fcf7
leticiafdepaula <leticiaferreira150998@gmail.com>
2024-08-06 20:01:59 -0300

    atualizando produtos no banco de dados

--- src/main/java/com/lvendas/gestao_vendas/controlador/CategoriaControlador.java
+++ src/main/java/com/lvendas/gestao_vendas/controlador/CategoriaControlador.java
@@ -1,13 +1,15 @@
 package com.lvendas.gestao_vendas.controlador;
 
 import com.lvendas.gestao_vendas.entidade.Categoria;
-import com.lvendas.gestao_vendas.servico.CategoriaServico;
+import com.lvendas.gestao_vendas.entidade.Produto;
+import com.lvendas.gestao_vendas.servico.ProdutoServico;
 import io.swagger.annotations.ApiOperation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 import java.util.Optional;
 
@@ -15,35 +17,36 @@ import java.util.Optional;
 @RequestMapping("/categoria")
 public class CategoriaControlador {
     @Autowired
-    private CategoriaServico categoriaServico;
+    private ProdutoServico produtoServico;
 
     @GetMapping
     public List<Categoria> ListarTodas() {
         return categoriaServico.listarTodas ();
     }
 
+    @ApiOperation (value = "listar por codigo", nickname = "buscarPorCodigo")
     @GetMapping("/{codigo}")
     public ResponseEntity<Optional<Categoria>> buscarPorId(@PathVariable Long codigo){
-        Optional<Categoria> categoria = categoriaServico.buscarPorCodigo (codigo);
+        Optional<Categoria> categoria = produtoServico.buscarPorCodigo (codigo);
         return categoria.isPresent () ? ResponseEntity.ok (categoria) : ResponseEntity.notFound ().build ();
     }
-
-    @PostMapping
-    public ResponseEntity<Categoria> salvar(@Valid  @RequestBody Categoria categoria) {
-        Categoria categoriaSalvar = categoriaServico.salvar (categoria);
+     @ApiOperation (value = "Salvar", nickname = "SalvarProduto")
+     @PostMapping
+     public ResponseEntity<Categoria> salvar(@Valid  @RequestBody Categoria categoria) {
+        Categoria categoriaSalvar = produtoServico.salvar (categoria);
         return ResponseEntity.status (HttpStatus.CREATED).body (categoriaSalvar);
     }
-
-    @PostMapping("/{codigo}")
-    public ResponseEntity<Categoria> atualizar(@Valid @PathVariable Long codigo, @RequestBody Categoria categoria) {
-         return ResponseEntity.ok (categoriaServico.atualizar (codigo, categoria));
+    @ApiOperation (value = "Atualizar", nickname = "AtualizarProduto")
+    @PostMapping("/{codigoProduto}")
+    public ResponseEntity<Produto> atualizar(@Valid @PathVariable Long codigoCategoria, @PathVariable Long codigoProduto, @Valid @RequestBody Produto produto) {
+    return ResponseEntity.ok (produtoServico.Atualizar (codigoCategoria, codigoProduto,produto));
     }
 
     @ApiOperation (value = "deletar")
     @DeleteMapping("/{codigo}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long codigo) {
-        categoriaServico.deletar (codigo);
+        produtoServico.deletar (codigo);
 
     }
 }
--- src/main/java/com/lvendas/gestao_vendas/servico/ProdutoServico.java
+++ src/main/java/com/lvendas/gestao_vendas/servico/ProdutoServico.java
@@ -3,7 +3,9 @@ package com.lvendas.gestao_vendas.servico;
 import com.lvendas.gestao_vendas.entidade.Produto;
 import com.lvendas.gestao_vendas.excecao.RegraNegocioException;
 import com.lvendas.gestao_vendas.repositorio.ProdutoRepositorio;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,12 +24,24 @@ public class ProdutoServico {
     public Optional<Produto> buscarPorCodigo(Long codigo, Long codigoCategoria) {
         return produtoRepositorio.buscarPorCodigo(codigo, codigoCategoria);
     }
-
     public Produto salvar (Produto produto) {
         validarCategoriaDoProdutoExiste (produto.getCategoria ().getCodigo ());
         validarProdutoDuplicado (produto);
         return produtoRepositorio.save (produto);
-
+    }
+    public Produto Atualizar(Long codigoCategoria, Long codigoProduto, Produto produto) {
+     Produto produtoSalvar = validarProdutoExiste(codigoProduto, codigoCategoria);
+        validarProdutoDuplicado (produto);
+        validarCategoriaDoProdutoExiste (codigoCategoria);
+        BeanUtils.copyProperties (produtoSalvar, produto, "codigo");
+        return produtoRepositorio.save (produtoSalvar);
+    }
+    private Produto validarProdutoExiste(Long codigoProduto, Long codigoCategoria) {
+        Optional<Produto> produto = buscarPorCodigo (codigoProduto, codigoCategoria);
+        if (produto.isEmpty ()) {
+            throw new EmptyResultDataAccessException (1);
+        }
+    return produto.get ();
     }
 
     private void validarProdutoDuplicado(Produto produto) {
@@ -44,4 +58,6 @@ public class ProdutoServico {
     }
     }
 
+    public void deletar(Long codigo) {
+    }
 }
